fae009d22724d367fe007bbcc75b860d0d4fd3ad
Ramilison Michel Heather <hei.michel.8@gmail.com>
2025-07-16 11:48:18 +0300

    feat: add request counter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const PORT = process.env.PORT || "8080";
 const express = require("express");
+const { countAllRequests } = require("./monitoring");
 const app = express();
+
+app.use(countAllRequests());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
